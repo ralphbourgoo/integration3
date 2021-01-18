f09de5d5fe819f806a623d20a89b57e626a8d734
Ralph Bourgoo <ralphbourgoo@Ralphs-MacBook-Pro.local>
2021-01-18 22:28:36 +0100

    HTML Start Page

---
+++ src/view/template/startpage.php
@@ -0,0 +1,11 @@
+<main>
+  <h1>Start Page</h1>
+  <section>
+    <img src="" alt="">
+    <h2>De Luchtballon</h2>
+    <p>Altijd al je eigen luchtballon willen maken ? Ter voorbereiding van Makers Faire Ghent 2021 bieden we een gratis tutorial aan. Ga meteen van start en maak jou eigen luchtballon !</p>
+    <div>
+      <a href="">Start Tutorial</a>
+    </div>
+  </section>
+</main>
