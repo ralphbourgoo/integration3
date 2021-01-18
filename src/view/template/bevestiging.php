b435e4e178545f9dec5ed8e49d046f464bbf96df
Ralph Bourgoo <ralphbourgoo@Ralphs-MacBook-Pro.local>
2021-01-18 22:11:59 +0100

    HTML Bevestigings Page

---
+++ src/view/template/bevestiging.php
@@ -0,0 +1,13 @@
+<main>
+  <h1>Bevestigings Page</h1>
+  <section>
+    <img src="" alt="">
+    <h2>Bedankt voor je bestelling</h2>
+    <p>Bedankt voor je bestelling. Een ordersbevestiging is vertuurd naar ralph.bourgoo@bourgoo.be.</p>
+    <p>Verder winkelen?</p>
+    <div>
+      <a href="">terug naar home</a>
+      <a href="">Verder Winkelen</a>
+    </div>
+  </section>
+</main>
