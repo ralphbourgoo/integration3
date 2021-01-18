4a552a883dc973f724ee26b37c520bfd5acffa34
Ralph Bourgoo <ralphbourgoo@Ralphs-MacBook-Pro.local>
2021-01-18 22:23:58 +0100

    HTML Checklist Page

---
+++ src/view/template/checklist.php
@@ -0,0 +1,40 @@
+<main>
+  <h1>Checklist Page</h1>
+  <section>
+    <h2>Heb je alles?</h2>
+    <p>Controleer of je alle items hebt in de toolkit. Als je zeker ben dat alles aanwezig is, kunnen we beginnen aan de tutorial. Laten we beginnen !</p>
+    <div>
+      <div>
+        <img src="" alt="">
+        <p>Ballon</p>
+      </div>
+      <div>
+        <img src="" alt="">
+        <p>4 Touwen</p>
+      </div>
+      <div>
+        <img src="" alt="">
+        <p>Mand</p>
+      </div>
+      <div>
+        <img src="" alt="">
+        <p>Test Pop</p>
+      </div>
+      <div>
+        <img src="" alt="">
+        <p>Buis</p>
+      </div>
+      <div>
+        <img src="" alt="">
+        <p>Haardroger</p>
+      </div>
+      <div>
+        <img src="" alt="">
+        <p>Pomp</p>
+      </div>
+    </div>
+    <div>
+      <a href="">laten we beginnen</a>
+    </div>
+  </section>
+</main>
