38f8796af60507fa2c4b5371b333e06d94ddc203
Ralph Bourgoo <ralphbourgoo@Ralphs-MacBook-Pro.local>
2021-01-24 15:11:22 +0100

    HTML & CSS checklistpage

--- src/view/template/checklist.php
+++ src/view/template/checklist.php
@@ -1,40 +1,42 @@
 <main>
-  <h1>Checklist Page</h1>
-  <section>
-    <h2>Heb je alles?</h2>
-    <p>Controleer of je alle items hebt in de toolkit. Als je zeker ben dat alles aanwezig is, kunnen we beginnen aan de tutorial. Laten we beginnen !</p>
-    <div>
-      <div>
-        <img src="" alt="">
-        <p>Ballon</p>
+  <h1 class="hidden">Checklist Page</h1>
+  <section class="start_wrapper">
+    <div class="checklist_header">
+      <h2 class="subtitle">Heb je alles?</h2>
+      <p class="text center max"><span>Controleer</span> of je alle items hebt in de toolkit. Als je zeker ben dat alles aanwezig is, kunnen we beginnen aan de tutorial. Laten we beginnen !</p>
+    </div>
+    <div class="checklist_wrapper">
+      <div class="checkitem_wrapper">
+        <img class="check_img" src="assets/img/checklist/balloon.svg" alt="ballon">
+        <p class="label">Ballon</p>
       </div>
-      <div>
-        <img src="" alt="">
-        <p>4 Touwen</p>
+      <div class="checkitem_wrapper">
+        <img class="check_img" src="assets/img/checklist/ropes.svg" alt="ropes">
+        <p class="label">4 Touwen</p>
       </div>
-      <div>
-        <img src="" alt="">
-        <p>Mand</p>
+      <div class="checkitem_wrapper">
+        <img class="check_img" src="assets/img/checklist/mand.svg" alt="mand">
+        <p class="label">Mand</p>
       </div>
-      <div>
-        <img src="" alt="">
-        <p>Test Pop</p>
+      <div class="checkitem_wrapper">
+        <img class="check_img" src="assets/img/checklist/buis.svg" alt="buis">
+        <p class="label">Buis</p>
       </div>
-      <div>
-        <img src="" alt="">
-        <p>Buis</p>
+      <div class="checkitem_wrapper">
+        <img class="check_img" src="assets/img/checklist/robot.svg" alt="test pop">
+        <p class="label">Test Pop</p>
       </div>
-      <div>
-        <img src="" alt="">
-        <p>Haardroger</p>
+      <div class="checkitem_wrapper">
+        <img class="check_img" src="assets/img/checklist/hairdryer.svg" alt="hairdryer">
+        <p class="label">Haardroger</p>
       </div>
-      <div>
-        <img src="" alt="">
-        <p>Pomp</p>
+      <div class="checkitem_wrapper">
+        <img class="check_img" src="assets/img/checklist/pomp.svg" alt="pomp">
+        <p class="label">Pomp</p>
       </div>
     </div>
-    <div>
-      <a href="">laten we beginnen</a>
+    <div class="button_wrapper">
+      <a class="button" href="index.php?page=tutorial">laten we beginnen</a>
     </div>
   </section>
 </main>
